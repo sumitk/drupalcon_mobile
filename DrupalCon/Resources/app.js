161d6173d1f733a89f0e57e8db78d05b371d27bc
Larry Garfield <garfield@palantir.net>
2010-12-16 11:10:24 -0600

    Restore build directory, with proper gitignore configuration.
    It looks like Titanium even created one for us. Interesting.

--- DrupalCon/Resources/app.js
+++ DrupalCon/Resources/app.js
@@ -1,6 +1,7 @@
 // this sets the background color of the master UIView (when there are no windows/tab groups on it)
 Titanium.UI.setBackgroundColor('#000');
 
+/*
 // create tab group
 var tabGroup = Titanium.UI.createTabGroup();
 
@@ -8,11 +9,11 @@ var tabGroup = Titanium.UI.createTabGroup();
 //
 // create base UI tab and root window
 //
-var win1 = Titanium.UI.createWindow({  
+var win1 = Titanium.UI.createWindow({
     title:'Tab 1',
     backgroundColor:'#fff'
 });
-var tab1 = Titanium.UI.createTab({  
+var tab1 = Titanium.UI.createTab({
     icon:'KS_nav_views.png',
     title:'Tab 1',
     window:win1
@@ -31,11 +32,11 @@ win1.add(label1);
 //
 // create controls tab and root window
 //
-var win2 = Titanium.UI.createWindow({  
+var win2 = Titanium.UI.createWindow({
     title:'Tab 2',
     backgroundColor:'#fff'
 });
-var tab2 = Titanium.UI.createTab({  
+var tab2 = Titanium.UI.createTab({
     icon:'KS_nav_ui.png',
     title:'Tab 2',
     window:win2
@@ -56,9 +57,31 @@ win2.add(label2);
 //
 //  add tabs
 //
-tabGroup.addTab(tab1);  
-tabGroup.addTab(tab2);  
+tabGroup.addTab(tab1);
+tabGroup.addTab(tab2);
 
 
 // open tab group
 tabGroup.open();
+
+*/
+
+var win1 = Titanium.UI.createWindow({
+  title:'Tab 1',
+  backgroundColor:'#000'
+});
+var tab1 = Titanium.UI.createTab({
+  icon:'KS_nav_views.png',
+  title:'Tab 1',
+  window:win1
+});
+
+var label1 = Titanium.UI.createLabel({
+  color:'#999',
+  text:'I am Window 1',
+  font:{fontSize:20,fontFamily:'Helvetica Neue'},
+  textAlign:'center',
+  width:'auto'
+});
+
+win1.open(label1);
