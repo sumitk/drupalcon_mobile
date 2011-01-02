84c62d396da2f70d7187f4cde04c39c7d7df21a0
Larry Garfield <larry@garfieldtech.com>
2011-01-01 19:18:37 -0600

    Initial cut at the DB routing object. As yet untested since there's no connection object yet.

---
+++ DrupalCon/Resources/drupal/db.js
@@ -0,0 +1,177 @@
+// Include the main Drupal library.
+if (!Drupal) {
+  Ti.include('drupal.js');
+}
+
+/**
+ * Define a new library for Drupal Entity storage.
+ */
+Drupal.db = {
+
+  /**
+   * Flag to indicate a query call should simply return NULL.
+   *
+   * This is used for queries that have no reasonable return value anyway, such
+   * as INSERT statements to a table without a serial primary key.
+   */
+  RETURN_NULL: 0,
+
+  /**
+   * Flag to indicate a query call should return the prepared statement.
+   */
+  RETURN_STATEMENT: 1,
+
+  /**
+   * Flag to indicate a query call should return the number of affected rows.
+   */
+  RETURN_AFFECTED: 2,
+
+  /**
+   * Flag to indicate a query call should return the "last insert id".
+   */
+  RETURN_INSERT_ID: 3,
+    
+  /**
+   * An nested array of all active connections. It is keyed by database name
+   *
+   * @var array
+   */
+  connections: [],
+
+  /**
+   * The key of the currently active database connection.
+   *
+   * @var string
+   */
+  activeKey: 'default',
+  
+  /**
+   * A processed copy of the database connection information from settings.php.
+   *
+   * @var array
+   */
+  databaseInfo: null,
+
+  /**
+   * Gets the connection object for the specified database key.
+   *
+   * @param $key
+   *   The database connection key. Defaults to NULL which means the active key.
+   *
+   * @return DatabaseConnection
+   *   The corresponding connection object.
+   */
+  getConnection: function(key) {
+    if (!key) {
+      key = this.activeKey;
+    }
+
+    if (!key) {
+      // By default, we want the active connection, set in setActiveConnection.
+      key = this.activeKey;
+    }
+
+    if (!this.connections[key]) {
+      // If necessary, a new connection is opened.
+      this.connections[key] = this.openConnection(key);
+    }
+    return this.connections[key];
+  },
+
+  /**
+   * Determines if there is an active connection.
+   *
+   * Note that this method will return FALSE if no connection has been
+   * established yet, even if one could be.
+   *
+   * @return
+   *   TRUE if there is at least one database connection established, FALSE
+   *   otherwise.
+   */
+  isActiveConnection: function() {
+    return (this.connections.length > 0);
+  },
+
+  /**
+   * Sets the active connection to the specified key.
+   *
+   * @return
+   *   The previous database connection key. If the requested
+   *   key was not registered, null is returned and no changes
+   *   are made.
+   */
+  setActiveConnection: function(key) {
+    if (!key) {
+      key = 'default';
+    }
+    
+    if (this.databaseInfo[key]) {
+      var oldKey = this.activeKey;
+      this.activeKey = key;
+      return oldKey;
+    }
+  },
+
+  /**
+   * Adds database connection information for a given key.
+   *
+   * In practice, all this method does is register a database name that can 
+   * be used. It is mostly to parallel the Drupal-side method of the same
+   * name, which by necessity is much more complex. In our case we are dealing
+   * only with single-use SQLite databases so this is just to register a name
+   * as openable.
+   *
+   * @param key
+   *   The database key.
+   */
+  addConnectionInfo: function(key) {
+    this.databaseInfo[key] = key;
+  },
+  
+  /**
+   * Opens a connection to the server specified by the given key.
+   *
+   * @param $key
+   *   The database connection key, as specified in settings.php. The default is
+   *   "default".
+   *
+   * @throws DatabaseConnectionNotDefinedException
+   * @throws DatabaseDriverNotSpecifiedException
+   */
+  openConnection: function(key) {
+    if (!key) {
+      key = 'default';
+    }
+
+    // If the requested database does not exist then it is an unrecoverable
+    // error.
+    if (!this.databaseInfo[key]) {
+      // @todo Turn this into a proper exception object, or whatever is appropriate in Javascript.
+      throw new Error('The specified database connection is not defined: ' + key);
+    }
+    
+    var newConnection = new Drupal.db.Connection(key);
+
+    return newConnection;
+  },
+
+  /**
+   * Closes a connection to the server specified by the given key and target.
+   *
+   * @param $target
+   *   The database target name.  Defaults to NULL meaning that all target
+   *   connections will be closed.
+   * @param $key
+   *   The database connection key. Defaults to NULL which means the active key.
+   */
+  closeConnection: function(key) {
+    // Gets the active connection by default.
+    if (!key) {
+      key = this.activeKey;
+    }
+    
+    this.connections[key].close();
+    this.connections[key] = undefined;
+  }
+};
+
