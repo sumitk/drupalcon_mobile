d18ac5bc68e00f2d8e22640b427cfc541cb503d2
Patrick Teglia <pteglia@Ubundio.(none)>
2011-01-02 22:41:58 -0800

    Adding preferences.js, which saves user and pass to prefs.  Also modified app.js to include a button that will verify the preference get ability.

--- DrupalCon/Resources/app.js
+++ DrupalCon/Resources/app.js
@@ -18,33 +18,47 @@ var label1 = Titanium.UI.createLabel({
 });
 mainWindow.add(label1);
 
-// Menu and settings handling is totally different between Android and iOS,
-// so we have to fork the logic.  Bah.  Possibly clean this up later.
-if (Titanium.Platform.osname == 'android') {
-  mainWindow.activity.onCreateOptionsMenu = function(e) {
-    var menu = e.menu;
+// Testing fetching preferences from the preferences.js file
+var checkButton = Titanium.UI.createButton({
+  title:'Check user/pass',
+  top: 10,
+  width:300,
+  height: 40
+});
+checkButton.addEventListener('click',function(e){
+  var pass = Titanium.App.Properties.getString("sitePassword");
+  var user = Titanium.App.Properties.getString("siteUsername");
+  alert("User: " + user + " and Pass: " + pass);
+});
+mainWindow.add(checkButton);
 
-    var m1 = menu.add({ title : 'Settings' });
-    m1.addEventListener('click', function(e) {
-      Titanium.UI.Android.openPreferences();
-    });
-    var m2 = menu.add({ title : 'Update sessions' });
-    m2.addEventListener('click', function(e) {
-      Ti.API.info("Update button was clicked.");
-      // Do stuff here to test downloading sessions.
-      Ti.API.info("Requesting new service object.");
-      var service = Drupal.createConnection({endpointUrl: 'http://chicago2011.garfield.sandbox/mobile/test'});
-      service.loadHandler = function() {
-        Ti.API.info("Data was loaded, called from custom handler.");
-      };
-      Ti.API.info("Making request.");
-      service.request({method: 'GET', query: 'node/464', format: 'json'});
-    });
-  };
-}
-else if (Titanium.Platform.name == 'iPhone OS') {
+mainWindow.activity.onCreateOptionsMenu = function(e) {
+  var menu = e.menu;
 
-}
+  var m1 = menu.add({ title : 'Settings' });
+  m1.addEventListener('click', function(e) {
+    Ti.API.info("Clicked Settings.");
+    var preferencesWindow = Titanium.UI.createWindow({
+      url:'preferences.js',
+      title:'Preferences',
+      backgroundColor: '#000'
+    });
+    preferencesWindow.open({modal:true});
+    
+  });
+  var m2 = menu.add({ title : 'Update sessions' });
+  m2.addEventListener('click', function(e) {
+    Ti.API.info("Update button was clicked.");
+    // Do stuff here to test downloading sessions.
+    Ti.API.info("Requesting new service object.");
+    var service = Drupal.createConnection({endpointUrl: 'http://chicago2011.garfield.sandbox/mobile/test'});
+    service.loadHandler = function() {
+      Ti.API.info("Data was loaded, called from custom handler.");
+    };
+    Ti.API.info("Making request.");
+    service.request({method: 'GET', query: 'node/464', format: 'json'});
+  });
+};
 
 
 mainWindow.open();
---
+++ DrupalCon/Resources/preferences.js
@@ -0,0 +1,67 @@
+var win = Titanium.UI.currentWindow;
+var android = Ti.Platform.name == 'android';
+var data = [];
+
+var labelRow = Ti.UI.createTableViewRow();
+var inputRow = Ti.UI.createTableViewRow();
+var buttonRow = Ti.UI.createTableViewRow();
+
+var titleLabel = Titanium.UI.createLabel({
+    text:'Please enter your chicago2011.drupal.org username and password.',
+    height:'auto',
+    width:300,
+    color:'#eee',
+    textAlign:'left',
+    font:{fontSize:18},
+    top:5
+});
+labelRow.add(titleLabel);
+
+var username = Ti.UI.createTextField({
+  autocapitalization:Titanium.UI.TEXT_AUTOCAPITALIZATION_NONE,
+  width:300,
+  top:5,
+  height: android ? 45 : 35,
+  borderStyle:Titanium.UI.INPUT_BORDERSTYLE_ROUNDED,
+  hintText:'Username'
+});
+inputRow.add(username);
+
+var password = Ti.UI.createTextField({
+  autocapitalization:Titanium.UI.TEXT_AUTOCAPITALIZATION_NONE,
+  width:300,
+  top:android ? 65 : 55,
+  height:android ? 45 : 35,
+  borderStyle:Titanium.UI.INPUT_BORDERSTYLE_ROUNDED,
+  passwordMask:true,
+  hintText:'Password'
+});
+inputRow.add(password);
+
+var button = Titanium.UI.createButton({
+  title:'Save',
+  top: android ? 200 : 180,
+  width:300,
+  height: android ? 45 : 40
+});
+buttonRow.add(button);
+
+data.push(labelRow);
+data.push(inputRow);
+data.push(buttonRow);
+
+tableView = Titanium.UI.createTableView({ data:data });
+
+win.add(tableView);
+
+button.addEventListener('click',function(e){
+  var pass = password.value ? password.value : 'no password';
+  var user = username.value ? username.value : 'no username';
+  Ti.API.info(user + ':' + pass);
+  
+  Titanium.App.Properties.setString("siteUsername",user);
+  Titanium.App.Properties.setString("sitePassword",pass);
+  Titanium.UI.createAlertDialog({title:'Preferences',message:'Your preferences have been saved.'}).show();
+  win.close();
+
+});
