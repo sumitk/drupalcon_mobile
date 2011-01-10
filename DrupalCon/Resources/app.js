58311f6e6c674023405bd70c470fd3fa0de616d4
Larry Garfield <larry@garfieldtech.com>
2011-01-09 23:35:54 -0600

    Use "main" for the DB connection, since "default" is a reserved word and we want everything to match up.

--- DrupalCon/Resources/app.js
+++ DrupalCon/Resources/app.js
@@ -12,7 +12,7 @@ Ti.include('drupalcon/entity.js');
 
 // Define our connection information.  This is very similar to the DB layer's
 // $databases array in settings.php.
-Drupal.services.addConnectionInfo('default', {
+Drupal.services.addConnectionInfo('main', {
   endpointUrl: 'http://chicago2011.drupal.org/services/mobile',
   user: '',
   pass: ''
