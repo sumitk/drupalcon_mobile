e245c0c7299336156b32e753412785dda462b6b1
Pat Teglia <pteglia@Patrick-Teglias-MacBook-Pro.local>
2011-01-10 15:44:45 -0600

    refs #1107 - Working on twitter services.

--- DrupalCon/Resources/app.js
+++ DrupalCon/Resources/app.js
@@ -4,9 +4,10 @@ Titanium.UI.setBackgroundColor('#000');
 // Include the Drupal connection libraries.
 Ti.include("drupal/drupal.js");
 Ti.include("drupal/services.js");
-
 Ti.include('drupal/db.js');
 Ti.include('drupal/entity.js');
+Ti.include('lib/twitter_services.js');
+
 
 // Define our connection information.  This is very similar to the DB layer's
 // $databases array in settings.php.
@@ -15,45 +16,6 @@ Drupal.services.addConnectionInfo('default', {
   user: '',
   pass: ''
 });
-
-// Check for new tweets
-setInterval (function() {
-  var screen_name = 'drupalcon';
-  var net = Titanium.Network;
-  var up = net.online;
-  
-  // Only check if the network is up.
-  if (up) {
-    Ti.API.info("In the up portion of the setInterval twitter check.");
-    var xhr = Ti.Network.createHTTPClient();
-    xhr.timeout = 1000;
-    xhr.open("GET","http://api.twitter.com/1/statuses/user_timeline.json?screen_name="+screen_name+"&count=1");
-    xhr.onload = function()
-    {
-      try
-      {
-        var tweets = eval('('+this.responseText+')');
-        for (var c=0;c<tweets.length;c++){
-          var tweet = tweets[c].text;
-        }
-        // Updated?  Then tell the world about it!
-        var lastTweet = Titanium.App.Properties.getString("lastTweet");
-        if (tweet != lastTweet) {
-          var n = Ti.UI.createNotification({message:"New @drupalcon tweets!"});
-          n.duration = Ti.UI.NOTIFICATION_DURATION_LONG;
-          n.offsetX = 50;
-          n.offsetY = 25;
-          n.show();
-        }
-   			Titanium.App.Properties.setString("lastTweet",tweet);
-      }
-      catch(E){
-        alert(E);
-      }
-    }
-   	xhr.send();
-	}
-},1000);
 
 // create tab group
 var tabGroup = Titanium.UI.createTabGroup({id:'tabGroup1'});
---
+++ DrupalCon/Resources/lib/twitter_services.js
@@ -0,0 +1,38 @@
+// Check for new tweets
+setInterval (function() {
+  var screen_name = 'drupalcon';
+  var net = Titanium.Network;
+  var up = net.online;
+  
+  // Only check if the network is up.
+  if (up) {
+    Ti.API.info("In the up portion of the setInterval twitter check.");
+    var xhr = Ti.Network.createHTTPClient();
+    xhr.timeout = 1000;
+    xhr.open("GET","http://api.twitter.com/1/statuses/user_timeline.json?screen_name="+screen_name+"&count=1");
+    xhr.onload = function()
+    {
+      try
+      {
+        var tweets = eval('('+this.responseText+')');
+        for (var c=0;c<tweets.length;c++){
+          var tweet = tweets[c].text;
+        }
+        // Updated?  Then tell the world about it!
+        var lastTweet = Titanium.App.Properties.getString("lastTweet");
+        if (tweet != lastTweet) {
+          var n = Ti.UI.createNotification({message:"New @drupalcon tweets!"});
+          n.duration = Ti.UI.NOTIFICATION_DURATION_LONG;
+          n.offsetX = 50;
+          n.offsetY = 25;
+          n.show();
+        }
+   			Titanium.App.Properties.setString("lastTweet",tweet);
+      }
+      catch(E){
+        alert(E);
+      }
+    }
+   	xhr.send();
+	}
+},1000);
