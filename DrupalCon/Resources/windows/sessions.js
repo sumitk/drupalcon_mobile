aba420b730d77cc5ebb99761ba0e0ae67666ecd3
Pat Teglia <pteglia@Patrick-Teglias-MacBook-Pro.local>
2011-02-01 21:25:05 -0800

    refs #1395 - Dividing the day results by time.

--- DrupalCon/Resources/windows/sessions.js
+++ DrupalCon/Resources/windows/sessions.js
@@ -9,27 +9,39 @@
     rootPath+"drupal/entity.js"
   );
 
-
   Ti.API.info('Start of sessions.js: ' + Drupal.getObjectProperties(Drupal.db.connectionInfo));
 
   var win = Titanium.UI.currentWindow;
-
+  var lastTime = '';
   var data = [];
+
   var conn = Drupal.db.getConnection('main');
-  var rows = conn.query("SELECT nid, title, changed, start_date, end_date FROM node WHERE start_date >= ? AND end_date <= ? ORDER BY nid, changed", [win.start_date, win.end_date]);
+  var rows = conn.query("SELECT nid, title, changed, start_date, end_date FROM node WHERE start_date >= ? AND end_date <= ? ORDER BY start_date, nid", [win.start_date, win.end_date]);
 
   while (rows.isValidRow()) {
+    // If it is a new time, start a new section.
+    if (lastTime == '' || rows.fieldByName('start_date') != lastTime) {
+      lastTime = rows.fieldByName('start_date');
+      data.push({
+        title: lastTime.substr(11,8),
+        hasChild: false,
+        backgroundColor:'#7187A4',
+        color:'#fff'
+      })
+    }
     data.push({
       title: rows.fieldByName('title'),
       hasChild: true,
       selectedColor: '#fff',
       start_date: rows.fieldByName('start_date'),
-      end_date: rows.fieldByName('end_date')
+      end_date: rows.fieldByName('end_date'),
+      nid: rows.fieldByName('nid')
     });
-    Titanium.API.info('Nid: ' + rows.fieldByName('nid') + ', Start: ' + rows.fieldByName('start_date') + ', End: ' + rows.fieldByName('end_date')  + ', Changed: ' + rows.fieldByName('changed') + ', Title: ' + rows.fieldByName('title'));
+    //Titanium.API.info('Nid: ' + rows.fieldByName('nid') + ', Start: ' + rows.fieldByName('start_date') + ', End: ' + rows.fieldByName('end_date')  + ', Changed: ' + rows.fieldByName('changed') + ', Title: ' + rows.fieldByName('title'));
     rows.next();
   }
   rows.close();
+  Ti.API.info(data);
 
   // create table view
   var tableview = Titanium.UI.createTableView({
