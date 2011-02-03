0effb54c7f2a0a6b206a47788a2898b86b61289c
Larry Garfield <larry@garfieldtech.com>
2011-02-03 00:15:54 -0600

    refs #1395 Remove redundant code.

--- DrupalCon/Resources/windows/main.js
+++ DrupalCon/Resources/windows/main.js
@@ -48,69 +48,16 @@
     transition:Titanium.UI.iPhone.AnimationStyle.FLIP_FROM_LEFT
   });
 
-  //
-  //  TAB GROUP EVENTS
-  //
-  var messageWin = Titanium.UI.createWindow({
-    height:30,
-    width:250,
-    bottom:70,
-    borderRadius:10,
-    touchEnabled:false,
-    orientationModes : [
-      Titanium.UI.PORTRAIT,
-      Titanium.UI.UPSIDE_PORTRAIT,
-      Titanium.UI.LANDSCAPE_LEFT,
-      Titanium.UI.LANDSCAPE_RIGHT
-    ]
-  });
-  var messageView = Titanium.UI.createView({
-    id:'messageview',
-    height:30,
-    width:250,
-    borderRadius:10,
-    backgroundColor:'#000',
-    opacity:0.7,
-    touchEnabled:false
-  });
-
-  var messageLabel = Titanium.UI.createLabel({
-    id:'messagelabel',
-    text:'',
-    color:'#fff',
-    width:250,
-    height:'auto',
-    font:{
-      fontFamily:'Helvetica Neue',
-      fontSize:13
-    },
-    textAlign:'center'
-  });
-  messageWin.add(messageView);
-  messageWin.add(messageLabel);
-
-  //
-  // TAB EVENTS
-  //
-
   // tab group close event
   tabGroup.addEventListener('close', function(e) {
-    messageLabel.text = 'tab group close event';
-    messageWin.open();
-    setTimeout(function() 	{
-      messageWin.close({opacity:0,duration:500});
-      tabGroup.open();
-    }, 1000);
+    var messageWin = Drupal.createNoticeDialog('tab group close event');
+    messageWin.show(1000);
   });
-
 
   // tab group open event
   tabGroup.addEventListener('open', function(e) {
-    messageLabel.text = 'tab group open event';
-    messageWin.open();
-    setTimeout(function() 	{
-      messageWin.close({opacity:0,duration:500});
-    }, 1000);
+    var messageWin = Drupal.createNoticeDialog('tab group open event');
+    messageWin.show(1000);
   });
 
   Ti.addEventListener('drupal:entity:datastore:update_completed', function(e) {
