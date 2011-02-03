cedb1f2862078d532566ee2fd51db0aecc6aad7a
Larry Garfield <larry@garfieldtech.com>
2011-02-03 01:00:49 -0600

    refs #1395 Whitespace fixes.

--- DrupalCon/Resources/windows/SessionDetailWindow.js
+++ DrupalCon/Resources/windows/SessionDetailWindow.js
@@ -37,8 +37,8 @@
       var rows = Drupal.db.getConnection('main').query("SELECT data,full_name FROM user WHERE name = ?", [instructors[i]]);
       while (rows.isValidRow()) {
         presenterData.push({
-          'data':JSON.parse(rows.fieldByName('data')),
-          'fullName':rows.fieldByName('full_name')
+          'data': JSON.parse(rows.fieldByName('data')),
+          'fullName': rows.fieldByName('full_name')
         });
         rows.next();
       }
@@ -48,55 +48,55 @@
     // Build the page:
     var tvData = [];
     var tv = Ti.UI.createTableView({
-      minRowHeight:50,
-      textAlign:'left'
+      minRowHeight: 50,
+      textAlign: 'left'
     });
     var row = Ti.UI.createTableViewRow({height:'auto',className:"row"});
 
     var textView = Ti.UI.createView({
-      height:'auto',
-      layout:'vertical',
-      backgroundColor:'#fff',
-      textAlign:'left',
-      color:'#000',
-      left:10,
-      top:10,
-      bottom:10,
-      right:10
+      height: 'auto',
+      layout: 'vertical',
+      backgroundColor: '#fff',
+      textAlign: 'left',
+      color: '#000',
+      left: 10,
+      top: 10,
+      bottom: 10,
+      right: 10
     });
 
     var l1 = Ti.UI.createLabel({
       text:cleanSpecialChars(sessionData.title),
-      font:{fontSize:24, fontWeight:'bold'},
-      backgroundColor:'#fff',
-      textAlign:'left',
-      color:'#000',
-      height:'auto',
-      width:itemWidth
+      font:{fontSize: 24, fontWeight: 'bold'},
+      backgroundColor: '#fff',
+      textAlign: 'left',
+      color: '#000',
+      height: 'auto',
+      width: itemWidth
     });
     textView.add(l1);
 
     for (var i in presenterData) {
       var presenterName = Ti.UI.createLabel({
-        text:presenterData[i].fullName,
-        backgroundColor:'#fff',
-        font:{fontSize:18, fontWeight:'bold'},
-        textAlign:'left',
-        color:'#000',
-        height:'auto',
-        width:itemWidth
+        text: presenterData[i].fullName,
+        backgroundColor: '#fff',
+        font:{fontSize: 18, fontWeight: 'bold'},
+        textAlign: 'left',
+        color: '#000',
+        height: 'auto',
+        width: itemWidth
       });
       textView.add(presenterName);
     }
 
     var room = Ti.UI.createLabel({
-      text:cleanSpecialChars(sessionData.room),
-      backgroundColor:'#fff',
-      textAlign:'left',
-      color:'#000',
-      top:10,
-      width:itemWidth,
-      height:'auto'
+      text: cleanSpecialChars(sessionData.room),
+      backgroundColor: '#fff',
+      textAlign: 'left',
+      color: '#000',
+      top: 10,
+      width: itemWidth,
+      height: 'auto'
     });
     textView.add(room);
 
