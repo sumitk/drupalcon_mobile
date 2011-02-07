df7032a8103d30d3d95bd9a19380c783cc797e91
Larry Garfield <larry@garfieldtech.com>
2011-02-06 23:57:02 -0600

    Add support for formatting a date to UTC format.

--- DrupalCon/Resources/drupal/drupal.js
+++ DrupalCon/Resources/drupal/drupal.js
@@ -50,20 +50,34 @@ Drupal.constructPrototype = function(o) {
  *
  * @param {Date} date
  *   The date object we want to format.
+ * @param {boolean} utc
+ *   True to format the date in UTC timezone, false to use the local timezone.
  * @return {string}
  *   The ISO formatted version of the date object.
  */
-Drupal.getISODate = function(date) {
+Drupal.getISODate = function(date, utc) {
 
   function pad(n) {return n < 10 ? '0' + n : n}
 
-  return date.getFullYear() + '-'
-    + pad(date.getMonth() + 1) + '-'
-    + pad(date.getDate()) + 'T'
-    + pad(date.getHours()) + ':'
-    + pad(date.getMinutes()) + ':'
-    + pad(date.getSeconds());
-};
+  if (utc) {
+    return date.getUTCFullYear() + '-'
+      + pad(date.getUTCMonth() + 1) + '-'
+      + pad(date.getUTCDate()) + 'T'
+      + pad(date.getUTCHours()) + ':'
+      + pad(date.getUTCMinutes()) + ':'
+      + pad(date.getUTCSeconds());
+  }
+  else {
+    return date.getFullYear() + '-'
+      + pad(date.getMonth() + 1) + '-'
+      + pad(date.getDate()) + 'T'
+      + pad(date.getHours()) + ':'
+      + pad(date.getMinutes()) + ':'
+      + pad(date.getSeconds());
+  }
+
+}
+
 
 
 Drupal.getObjectProperties = function(o) {
