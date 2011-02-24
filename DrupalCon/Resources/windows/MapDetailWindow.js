4917a894676cb26f73ce8e1654a03d9b8f3fd30f
Pat Teglia <pteglia@Patrick-Teglias-MacBook-Pro.local>
2011-02-23 23:24:38 -0800

    OMG I actually got it working (map zoom) in android.  DON'T BREATH ON IT

--- DrupalCon/Resources/windows/MapDetailWindow.js
+++ DrupalCon/Resources/windows/MapDetailWindow.js
@@ -12,94 +12,42 @@
 
     var mapDetailWindow = Titanium.UI.createWindow({
       id: 'mapDetailWindow',
+      width: 'auto',
+      height: 'auto',
       title: settings.mapName,
       backgroundColor: '#FFF',
       tabGroup: settings.tabGroup
     });
-//
-//    mapDetailWindow.addEventListener('focus', function() {
-//      // We should be showing the "tap to zoom" message here, but for some reason
-//      // it doesn't unshow from here. Eventually we may figure this out.
-//    });
-//
-//    var scrollView = Titanium.UI.createScrollView({
-//      contentWidth:'auto',
-//      contentHeight:'auto',
-//      top:0,
-//      showVerticalScrollIndicator:true,
-//      showHorizontalScrollIndicator:true,
-//      backgroundColor:'#031a28'
-//    });
-//    var view = Ti.UI.createView({
-//      backgroundImage:mapImageFileName,
-//      borderRadius:0,
-//      width:480,
-//      height:240,
-//      top:0
-//    });
-//    var center1 = null;
-//    var scaled1 = false;
-//
-//    var theZoomScale = 1.0;
-//    scrollView.addEventListener('scale', function(e){
-//      var currentScale = e.scale;
-//    });
-//
-//    scrollView.addEventListener('doubletap', function(e){
-//      var t = Titanium.UI.create2DMatrix();
-//
-//      if (!scaled1) {
-//        //dpm('center: ' + scrollView.center);
-//        t = t.scale(theZoomScale);
-//        view.width = 1000;
-//        view.height = 525;
-//        scrollView.contentWidth = 1000;
-//        scrollView.contentHeight = 525;
-//        scaled1 = true;
-//      }
-//      else {
-//        view.width = 500;
-//        view.height = 250;
-//        scrollView.contentWidth = 500;
-//        scrollView.contentHeight = 250;
-//        scaled1 = false;
-//      }
-//
-////
-////      if (currentScale > theZoomScale) {
-////        Ti.API.info("Zoomed at: " + e.x + ", " + e.y + " zoom: " + currentScale);
-////        scrollView.zoomScale = theZoomScale;
-////      }
-////      else {
-////        Ti.API.info("Not zoomed at: " + e.x + ", " + e.y + " zoom: " + currentScale);
-////        scrollView.scrollTo(e.x, e.y);
-////        scrollView.zoomScale = 2;
-////      }
-//    });
-//
-//    scrollView.add(view);
-//    mapDetailWindow.add(scrollView);
-//
-//    Drupal.createNoticeDialog('Double-Tap to Zoom.').show(3000);
 
+  //  I am so tired that I don't know why I am doing math like below.
+  //  This undoubtedly needs work.
+  var ht = mapDetailWindow.toImage().height;
   if (isAndroid()) {
-    var baseHTML = '<html><head>' +
-      '  <meta name="viewport" content="target-densitydpi=device-dpi, user-scalable=yes, width=device-width, initial-scale = 1.0, minimum-scale = 1.0, maximum-scale = 4.0" />' +
-      '</head><body class="maps"><div class="map">' +
-      '<div><img src="' + mapImageFileName + '" /></div>' +
-      '</body></html>';
+    var imageView = Ti.UI.createImageView({
+      image: mapImageFileName,
+      top: 0,
+      bottom: 0,
+      left: 0,
+      right: 0,
+      height: ht*2,
+      width: (ht*2)*1.5,
+      canScale: true
+    });
+    mapDetailWindow.add(imageView);
+
   }
   else {
     var baseHTML = '<html><head></head><body class="maps">' +
-      '  <meta name="viewport" content="target-densitydpi=device-dpi, user-scalable=yes, width=device-width, initial-scale = .25, minimum-scale = .25, maximum-scale = 4.0" />' +
+      '  <meta name="viewport" content="target-densityDpi=device-dpi, user-scalable=yes, width=device-width, initial-scale = .25, minimum-scale = .25, maximum-scale = 4.0" />' +
       '  <meta name="apple-mobile-web-app-capable" content="yes" />' +
       '<div class="map">' +
       '<div><img src="' + mapImageFileName + '" /></div>' +
       '</body></html>';
+    var web = Ti.UI.createWebView({scalesPageToFit:true});
+    mapDetailWindow.add(web);
+    web.html = baseHTML;
   }
-  var web = Ti.UI.createWebView({scalesPageToFit:true});
-  mapDetailWindow.add(web);
-  web.html = baseHTML;
+
 
     
 
