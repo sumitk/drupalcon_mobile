681dfff3636b2b0fa219aeb85deec3f3b3de4ae8
Larry Garfield <larry@garfieldtech.com>
2011-02-27 20:27:06 -0600

    Add static pages for Monday and Friday.

--- DrupalCon/Resources/windows/DayWindow.js
+++ DrupalCon/Resources/windows/DayWindow.js
@@ -5,14 +5,14 @@
 
   DrupalCon.ui.createDayWindow = function(tabGroup) {
 
-    // create table view data object
-    var data = [
-      //{title:'Monday, March 7', hasChild:true, color:'#000', backgroundColor:'#fff', backgroundSelectedColor:'#0779BE', start_date:'2011-03-07T00:00:00', end_date:'2011-03-08T00:00:00'},
-      {title:'Tuesday, March 8', hasChild:true, color:'#000', backgroundColor:'#fff', backgroundSelectedColor:'#0779BE', start_date:'2011-03-08T00:00:00', end_date:'2011-03-09T00:00:00'},
-      {title:'Wednesday, March 9', hasChild:true, color:'#000', backgroundColor:'#fff', backgroundSelectedColor:'#0779BE', start_date:'2011-03-09T00:00:00', end_date:'2011-03-10T00:00:00'},
-      {title:'Thursday, March 10', hasChild:true, color:'#000', backgroundColor:'#fff', backgroundSelectedColor:'#0779BE', start_date:'2011-03-10T00:00:00', end_date:'2011-03-11T00:00:00'}
-      //{title:'Friday, March 11', hasChild:true, color:'#000', backgroundColor:'#fff', backgroundSelectedColor:'#0779BE', start_date:'2011-03-11T00:00:00', end_date:'2011-03-12T00:00:00'}
-    ];
+    // Create table view data object.
+    var data = [];
+
+    data.push({title:'Monday, March 7', hasChild:true, color:'#000', backgroundColor:'#fff', backgroundSelectedColor:'#0779BE', scheduleListing: false, url: 'pages/2011-03-07.html'});
+    data.push({title:'Tuesday, March 8', hasChild:true, color:'#000', backgroundColor:'#fff', backgroundSelectedColor:'#0779BE', start_date:'2011-03-08T00:00:00', end_date:'2011-03-09T00:00:00', scheduleListing: true});
+    data.push({title:'Wednesday, March 9', hasChild:true, color:'#000', backgroundColor:'#fff', backgroundSelectedColor:'#0779BE', start_date:'2011-03-09T00:00:00', end_date:'2011-03-10T00:00:00', scheduleListing: true});
+    data.push({title:'Thursday, March 10', hasChild:true, color:'#000', backgroundColor:'#fff', backgroundSelectedColor:'#0779BE', start_date:'2011-03-10T00:00:00', end_date:'2011-03-11T00:00:00', scheduleListing: true});
+    data.push({title:'Friday, March 11', hasChild:true, color:'#000', backgroundColor:'#fff', backgroundSelectedColor:'#0779BE', scheduleListing: false, url: 'pages/2011-03-11.html'});
 
     var dayWindow = Titanium.UI.createWindow({
       id: 'win1',
@@ -38,12 +38,21 @@
       if (uiEnabled) {
         uiEnabled = false;
         var currentTab = (Ti.Platform.name == 'android') ? Titanium.UI.currentTab : dayWindow.tabGroup.activeTab;
-        currentTab.open(DrupalCon.ui.createSessionsWindow({
-          title: e.rowData.title,
-          start_date: e.rowData.start_date,
-          end_date: e.rowData.end_date,
-          tabGroup: currentTab
-        }), {animated:true});
+        if (e.rowData.scheduleListing) {
+          currentTab.open(DrupalCon.ui.createSessionsWindow({
+            title: e.rowData.title,
+            start_date: e.rowData.start_date,
+            end_date: e.rowData.end_date,
+            tabGroup: currentTab
+          }), {animated:true});
+        }
+        else {
+          currentTab.open(DrupalCon.ui.createHtmlWindow({
+            title: e.rowData.title,
+            url: e.rowData.url,
+            tabGroup: currentTab
+          }), {animated:true});
+        }
       }
     });
 
