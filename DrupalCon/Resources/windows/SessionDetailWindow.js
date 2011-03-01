9ae42ae71096f9545839832a09404d102c1d59ac
Larry Garfield <garfield@palantir.net>
2011-03-01 13:58:49 -0600

    Don't show the room info for the Field party, either.

--- DrupalCon/Resources/windows/SessionDetailWindow.js
+++ DrupalCon/Resources/windows/SessionDetailWindow.js
@@ -98,7 +98,12 @@
     // actually completely wrong. It's hacked in for the site display, but
     // wrong for the mobile app.  We do want to show rooms for the keynotes,
     // however, which is why we can't jus exclude schedule_items.
-    if (sessionData.room && sessionData.title !== 'Lunch' && sessionData.title !== 'Break') {
+    var skipRoom;
+    if (sessionData.title === 'Lunch' || sessionData.title === 'Break' || sessionData.title.indexOf('Party') !== -1) {
+      skipRoom = true;
+    }
+
+    if (sessionData.room && !skipRoom) {
       var room = Ti.UI.createLabel({
         text: sessionData.room.map(cleanSpecialChars).join(', '),
         font: {fontSize: 18, fontWeight: 'normal'},
