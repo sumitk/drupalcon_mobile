b923ac2e268810c2c115e831ab24621422084ca6
Pat Teglia <pteglia@Patrick-Teglias-MacBook-Pro.local>
2011-03-01 09:59:16 -0800

    Restoring db line

--- DrupalCon/Resources/app.js
+++ DrupalCon/Resources/app.js
@@ -34,7 +34,7 @@ Drupal.db.addConnectionInfo('main');
 
 // Pre-create the database.  This is a snapshot of the dataset taken shortly
 // prior to shipping.
-// Ti.Database.install('main.sql', 'main');
+Ti.Database.install('main.sql', 'main');
 
 // If we haven't created the tables yet, make empty ones to ensure that the
 // app doesn't crash.
